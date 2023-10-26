8f78a91148634fa2bb6667b49454f10922fa0fd6
chauhoang <chauhoang@CHAUHOANG9B1B>
2023-10-26 23:05:36 +0700

    MushRoom

--- SE102.011/MushRoom.h
+++ SE102.011/MushRoom.h
@@ -1,6 +1,11 @@
 #pragma once
 #include "GameObject.h"
 
-class CCMushRoom : public CGameObject {
-
+class CMushRoom : public CGameObject {
+	protected:
+		float ax, ay, startX;
+		int model;
+		virtual void Render();
+		virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObject);
+		virtual void GetBoundingBox(float &left, float &top, float &right, float &bottom);
 };
